1ced0289a85d3b3dbcd907708a76eaa735233e6f
tcbarret <tcbarret@uvm.edu>
2026-05-05 16:12:23 -0400

    Fix path construction

--- R/EntwinePDAL.R
+++ R/EntwinePDAL.R
@@ -186,13 +186,13 @@ buildPDALPipelineENTWINE <- function(
                         paste(pipelineOutputFileBaseName,
                         "_",
                         polys@data[thePoly, IDColumnLabel],
-                        ".json"), sep = ""))
+                        ".json", sep = "")))
     } else {
       jsonFile <- normalizePath(file.path(pipelineOutputFolder,
                         paste(basename(dirname(polys@data[thePoly, URLColumnLabel])),
                         "_",
                         polys@data[thePoly, IDColumnLabel],
-                        ".json"), sep = ""))
+                        ".json", sep = "")))
     }
 #     if (pipelineOutputFileBaseName != "") {
 #       jsonFile <- paste(pipelineOutputFolder, "\\",
